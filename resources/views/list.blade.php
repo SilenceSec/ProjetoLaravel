98f98088b32c01150730bd7b78e8e567c59f3520
João Victor <jhon4work@gmail.com>
2022-03-26 13:33:29 -0300

    Primeiro Commit

---
+++ resources/views/list.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Lista de programadores</h1>
+    <p>1.João</p></br>
+    <p>2.Ana</p></br>
+    <p>3.Victoria</p></br>
+    <p>4.Isa</p></br>
+    <p>5.Arthur</p></br>
+    <p>6.Miguel</p></br>
+    <p>7.Enzo</p></br>
+    <p>8.Carlos</p></br>
+
+</body>
+</html> 
